57bec0358f6b94d7018e8fef4391de2bbfd02f60
jessicaMarquess <jessica.marques.dev@gmail.com>
2024-12-18 10:03:47 -0300

    comprimindo gifs e add robots txt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,9 @@ import Image from 'next/image';
 import happyGif from '../../public/happy.gif'
 import { Button } from '@/components/ui/button';
 import { ArrowUpRight } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Home() {
-  const route = useRouter()
   return (
     <div className="max-w-5xl mx-auto p-4 mt-14 text-pink-600">
       <h1 className="font-mono text-4xl">
@@ -17,9 +16,12 @@ export default function Home() {
           Aqui você vai encontrar uma série de perguntas que eu mesma vou analisar. Mas não se preocupe! Se você chegou até aqui, é porque já tem potencial para um encontro comigo. Yay! Que felicidade.
         </p>
         <Image src={happyGif} alt='garota comemorando' className='size-fit' />
-        <Button variant="ghost" className='flex items-center text-xl mt-4' onClick={() => route.push('/form-page')}>
-          Vamos começar?! <ArrowUpRight className='size-12' />
-        </Button>
+
+        <Link href="/form-page">
+          <Button variant="ghost" className='flex items-center text-xl mt-4'>
+            Vamos começar?! <ArrowUpRight className='size-12' />
+          </Button>
+        </Link>
       </div>
     </div>
   );
